af568cefa63b4a693e035f1b1ac0c005ef30e0b2
Patrik Sandahl <patrik.sandahl@gmail.com>
2013-06-24 14:18:37 +0200

    A little more parsing.

--- src/ModuleParser.hs
+++ src/ModuleParser.hs
@@ -4,11 +4,31 @@ import Text.ParserCombinators.Parsec
 import Control.Applicative ((<$>), (<*>), (*>))
 
 data ModuleDef = ModuleDecl String
+               | MethodDecl String
+               | StaticDecl String String
                deriving Show
+
+-- Parse a module
+parse :: Parser [ModuleDef]
+parse = (:) <$> moduleDecl <*> many1 moduleBody
 
 -- Identify and return a module declaration
 moduleDecl :: Parser ModuleDef
 moduleDecl = ModuleDecl <$> between moduleKW whereKW cppId
+
+-- Identify and return a fragment of a module body
+moduleBody :: Parser ModuleDef
+moduleBody = try staticDecl
+             <|> try methodDecl
+             <?> "Method or static declaration"
+
+-- Identify and return a method declaration
+methodDecl :: Parser ModuleDef
+methodDecl = MethodDecl <$> (spaces *> cppId)
+
+-- Identify and return a static declaration
+staticDecl :: Parser ModuleDef
+staticDecl = StaticDecl <$> (spaces *> cppId) <*> (char '.' *> cppId)
 
 -- Identify the 'module' keyword
 moduleKW :: Parser ()
@@ -24,3 +44,7 @@ cppId = spaces *> ((:) <$> oneOf first <*> many (oneOf cont))
   where
     first = ['a'..'z']++['A'..'Z']++"_"
     cont  = first++['0'..'9']
+    
+-- Identify a '::'
+doubleColon :: Parser ()
+doubleColon = spaces *> string "::" *> return ()
