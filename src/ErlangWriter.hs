fe65c678bfb06bb87fd21698d5ac5f1ab1693bef
Patrik Sandahl <patrik.sandahl@gmail.com>
2013-06-25 16:02:55 +0200

    Full functionality for the Erlang generation.

--- src/ErlangWriter.hs
+++ src/ErlangWriter.hs
@@ -16,7 +16,11 @@ generateErlang :: [ModuleDef] -> StringWriter
 generateErlang (ModuleDecl moduleName:moduleBody) = do
   genModuleHeading
   genModuleName $ strToLower moduleName
+  genCommunicatorExports
   genModuleExports moduleBody
+  genMacroDefinitions
+  genStartStopFunctions
+  mapM_ genFunction moduleBody
   
 genModuleHeading :: StringWriter
 genModuleHeading = 
@@ -25,9 +29,15 @@ genModuleHeading =
 genModuleName :: String -> StringWriter    
 genModuleName m = tell $ "-module(" ++ m ++ ").\n\n"    
                  
+genCommunicatorExports :: StringWriter                  
+genCommunicatorExports = do
+  tell "%% API to start and stop the C++ communicator\n"
+  tell "-export([start/0,stop/0]).\n\n"
+                  
 genModuleExports :: [ModuleDef] -> StringWriter
 genModuleExports []         = return ()
 genModuleExports (def:defs) = do
+  tell "%% API for the system under test\n"
   tell "-export(["
   genModuleExport def
   mapM_ prependExportWithComma defs
@@ -43,5 +53,54 @@ genModuleExport def =
   where
     gen m p = tell $ (strToLower m) ++ "/" ++ show (length p)
 
+genMacroDefinitions :: StringWriter
+genMacroDefinitions = do
+  tell "%% Handy macros for C++ communication\n"
+  tell "-define(ExtProg, \"./TestMain\").\n"
+  tell "-define(CppComm, cpp_comm).\n\n"
+  
+genStartStopFunctions :: StringWriter
+genStartStopFunctions = do
+  tell "%% Functions for start and stop\n"
+  tell "start() ->\n"
+  tell "    ?CppComm:start_link(?ExtProg).\n"
+  tell "stop() ->\n"
+  tell "    ?CppComm:stop().\n\n"
+
+genFunction :: ModuleDef -> StringWriter
+genFunction def =
+  let
+    (f, ps) = case def of
+      (MethodDecl f ps)   -> (f, ps)
+      (StaticDecl _ f ps) -> (f, ps)
+    cs     = map shallConvertToBin ps
+  in
+   gen (strToLower f) (length cs) cs  
+  where
+    gen :: String -> Int -> [Bool] -> StringWriter
+    gen f l cs = do
+      tell $ f ++ "(" ++ formals l ++ ") ->\n"
+      tell $ "    ?CppComm:call({" ++ f ++ (cargs cs) ++ "}).\n\n"      
+      
+    formals :: Int -> String
+    formals 0 = ""
+    formals 1 = "Arg1"
+    formals n = foldl (\a b -> a ++ ",Arg" ++ show b) "Arg1" [2..n]
+    
+    cargs :: [Bool] -> String
+    cargs []     = ""
+    cargs (c:cs) = snd $ foldl (\(n, a) c -> (n+1, a ++ maybeConvertArg c n)) (2, (maybeConvertArg c 1)) cs
+
 strToLower :: String -> String
 strToLower = map toLower
+
+shallConvertToBin :: Param -> Bool
+shallConvertToBin (Ptr _) = False
+shallConvertToBin (Value v) =
+  case v of
+    String    -> True
+    otherwise -> False
+    
+maybeConvertArg :: Bool -> Int -> String
+maybeConvertArg False n = ",Arg" ++ show n
+maybeConvertArg True n  = ",list_to_binary(Arg" ++ show n ++ ")"
