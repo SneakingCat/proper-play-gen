155bcd772c56836870d2dd929572695a7f3e4879
Patrik Sandahl <patrik.sandahl@gmail.com>
2013-06-27 14:55:42 +0200

    More C++ code generation.h

--- src/CppWriter.hs
+++ src/CppWriter.hs
@@ -38,9 +38,22 @@ renderPrologue = do
   tell "// stdin and stdout. If tracing is needed from this program it\n"
   tell "// must be written to stderr or a separate file.\n"
   tell "int main() {\n"
+  tell "  erl_init(NULL, 0);\n"
+  tell "  ErlComm::Byte buf[BufSize];\n"
+  tell "  // Receive loop of messages from Erlang\n"
+  tell "  while (ErlComm::receive(buf) > 0) {\n\n"
+  tell "    // Decode the message tuple\n"
+  tell "    ETERM *tuple = erl_decode(buf);\n"
+  tell "    assert(ERL_IS_TUPLE(tuple));\n\n"
+  tell "    // Pick the first element of the tuple - the function to execute\n"
+  tell "    ETERM *func = erl_element(1, tuple);\n"
+  tell "    assert(ERL_IS_ATOM(func));\n\n"
   
 renderEpilogue :: StringWriter
 renderEpilogue = do
+  tell "    erl_free_compound(tuple);\n"
+  tell "    erl_free_term(func);\n"
+  tell "  }\n"
   tell "  return 0;\n"
   tell "}\n"
   
